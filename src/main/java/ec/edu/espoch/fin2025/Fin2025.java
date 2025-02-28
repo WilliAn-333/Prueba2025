4297607021837f6f10b2884343afb38b9ebbd73a
WilliAn-333 <www.pegazo23@gmail.com>
2025-02-27 22:55:01 -0500

    ...

---
+++ src/main/java/ec/edu/espoch/fin2025/Fin2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.fin2025;
+
+/**
+ *
+ * @author USER
+ */
+public class Fin2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
